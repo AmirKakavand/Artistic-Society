9e8e144642f40b12bf11f641e3a437454038b0c2
Kave Bahraman <sadegh.b218@gmail.com>
2022-07-28 22:43:26 +0430

    Compeltes song database methods

--- src/models/songs.rs
+++ src/models/songs.rs
@@ -1,6 +1,8 @@
 use super::song;
 use super::DatabaseResult;
 use super::PgConnection;
+use diesel::prelude::*;
+use diesel::result::Error;
 
 /// represent database song table
 #[derive(Queryable)]
@@ -16,17 +18,33 @@ pub struct Song {
 impl Song {
     /// adds a new song to database
     pub fn add(conn: &mut PgConnection, song: NewSong) -> DatabaseResult<Song> {
-        unimplemented!()
+        match diesel::insert_into(song::table)
+            .values(song)
+            .get_result::<Song>(conn)
+        {
+            Ok(song) => DatabaseResult::Successful(song),
+            Err(err) => panic!("Error!, message: {}", err),
+        }
     }
 
     /// gets a song from database
     pub fn get(conn: &mut PgConnection, song_id: i32) -> DatabaseResult<Song> {
-        unimplemented!()
+        use song::id;
+        match song::table.filter(id.eq(song_id)).load::<Song>(conn) {
+            Ok(mut song_vec) if !song_vec.is_empty() => {
+                DatabaseResult::Successful(song_vec.pop().unwrap())
+            }
+            Ok(_) => DatabaseResult::NotFound,
+            Err(err) => panic!("Error!, message: {}", err),
+        }
     }
 
     /// gets all songs from database
-    pub fn get_all(conn: &mut PgConnection, song_id: i32) -> DatabaseResult<Vec<Song>> {
-        unimplemented!()
+    pub fn get_all(conn: &mut PgConnection) -> DatabaseResult<Vec<Song>> {
+        match song::table.load::<Song>(conn) {
+            Ok(song_vec) => DatabaseResult::Successful(song_vec),
+            Err(err) => panic!("Error!, message: {}", err),
+        }
     }
 
     /// updates a song in database
@@ -35,22 +53,39 @@ impl Song {
         song_id: i32,
         update_song: NewSong,
     ) -> DatabaseResult<Song> {
-        unimplemented!()
+        use song::id;
+        match diesel::update(song::table.filter(id.eq(song_id)))
+            .set(update_song)
+            .get_result::<Song>(conn)
+        {
+            Ok(song) => DatabaseResult::Successful(song),
+            Err(Error::DatabaseError(_, _)) => DatabaseResult::NotFound,
+            Err(err) => panic!("Error!, message: {}", err),
+        }
     }
 
     /// removes a song from database
     pub fn delete(conn: &mut PgConnection, sing_id: i32) -> DatabaseResult<Song> {
-        unimplemented!()
+        use song::id;
+        match diesel::delete(song::table.filter(id.eq(sing_id))).get_result::<Song>(conn) {
+            Ok(song) => DatabaseResult::Successful(song),
+            Err(Error::DatabaseError(_, _)) => DatabaseResult::NotFound,
+            Err(err) => panic!("Error!, message: {}", err),
+        }
     }
 
     /// gets an artist's all songs
     pub fn get_artist_images(conn: &mut PgConnection, artist_id: i32) -> DatabaseResult<Vec<Song>> {
-        unimplemented!()
+        use song::artist;
+        match song::table.filter(artist.eq(artist_id)).load::<Song>(conn) {
+            Ok(song_vec) => DatabaseResult::Successful(song_vec),
+            Err(err) => panic!("Error!, message: {}", err),
+        }
     }
 }
 
 /// struct to insert data to song table
-#[derive(Insertable)]
+#[derive(Insertable, AsChangeset)]
 #[diesel(table_name = song)]
 pub struct NewSong {
     pub name: String,
