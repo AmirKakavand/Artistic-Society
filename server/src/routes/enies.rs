830f4e631b5da3fd4755e0000f9a2e9d67217a82
Kave Bahraman <sadegh.b218@gmail.com>
2022-07-29 11:38:26 +0430

    Fixes builder barrow and moved errors

--- server/src/routes/enies.rs
+++ server/src/routes/enies.rs
@@ -5,7 +5,7 @@ use rocket::serde::json::Json;
 /// GET to retrieve an image info
 #[get("/image")]
 pub fn get_image(mut conn: DbConn) -> Result<Json<Image>, Error> {
-    let errors = Error::build();
+    let mut errors = Error::build();
     let image = Image::get_random(&mut conn);
     if image.is_none() {
         errors.no_image();
@@ -17,7 +17,7 @@ pub fn get_image(mut conn: DbConn) -> Result<Json<Image>, Error> {
 /// GET to retrieve an image info
 #[get("/song")]
 pub fn get_song(mut conn: DbConn) -> Result<Json<Song>, Error> {
-    let errors = Error::build();
+    let mut errors = Error::build();
     let song = Song::get_random(&mut conn);
     if song.is_none() {
         errors.no_image();
@@ -28,7 +28,7 @@ pub fn get_song(mut conn: DbConn) -> Result<Json<Song>, Error> {
 
 #[derive(Responder)]
 #[response(status = 500, content_type = "application/json")]
-pub struct Error(&'static str);
+pub struct Error(String);
 
 impl Error {
     pub fn build() -> ErrorBuilder {
@@ -43,9 +43,9 @@ pub struct ErrorBuilder(Vec<&'static str>);
 
 impl ErrorBuilder {
     pub fn format_error_string(&self) -> String {
-        let temp_str = String::from("[");
-        for err in self.0 {
-            temp_str.push_str(format!("{}, ", err));
+        let mut temp_str = String::from("[");
+        for err in &self.0 {
+            temp_str.push_str(&format!("{}, ", err));
         }
         temp_str.push(']');
         temp_str
@@ -53,24 +53,24 @@ impl ErrorBuilder {
 
     pub fn finish(self) -> Result<(), Error> {
         if !self.0.is_empty() {
-            Err(Error::new(self.format_error_string()))?
+            Err(Error::new(self.format_error_string()))
+        } else {
+            Ok(())
         }
-        Ok(())
     }
 
-    pub fn is_valid(self) -> Result<Self, Error> {
+    pub fn is_valid(&self) -> Result<(), Error> {
         if !self.0.is_empty() {
-            self.finish()?;
+            Err(Error::new(self.format_error_string()))
+        } else {
+            Ok(())
         }
-        Ok(self)
     }
 
-    pub fn no_song(self) -> Self {
+    pub fn no_song(&mut self) {
         self.0.push("no song on disk!");
-        self
     }
-    pub fn no_image(self) -> Self {
+    pub fn no_image(&mut self) {
         self.0.push("no image on disk!");
-        self
     }
 }
