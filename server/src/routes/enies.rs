0f113e330a9e836fc9ba36283024e7bdf89359e6
Kave Bahraman <sadegh.b218@gmail.com>
2022-07-29 11:18:56 +0430

    Adds image and song GET routes

---
+++ server/src/routes/enies.rs
@@ -0,0 +1,76 @@
+use crate::db::DbConn;
+use crate::models::{Artist, Image, Song};
+use rocket::serde::json::Json;
+
+/// GET to retrieve an image info
+#[get("/image")]
+pub fn get_image(mut conn: DbConn) -> Result<Json<Image>, Error> {
+    let errors = Error::build();
+    let image = Image::get_random(&mut conn);
+    if image.is_none() {
+        errors.no_image();
+    }
+    errors.finish()?;
+    Ok(Json(image.unwrap()))
+}
+
+/// GET to retrieve an image info
+#[get("/song")]
+pub fn get_song(mut conn: DbConn) -> Result<Json<Song>, Error> {
+    let errors = Error::build();
+    let song = Song::get_random(&mut conn);
+    if song.is_none() {
+        errors.no_image();
+    }
+    errors.finish()?;
+    Ok(Json(song.unwrap()))
+}
+
+#[derive(Responder)]
+#[response(status = 500, content_type = "application/json")]
+pub struct Error(&'static str);
+
+impl Error {
+    pub fn build() -> ErrorBuilder {
+        ErrorBuilder(vec![])
+    }
+    pub fn new(errors: String) -> Self {
+        Error(format!("{{ \" errors \": {} }}", errors))
+    }
+}
+
+pub struct ErrorBuilder(Vec<&'static str>);
+
+impl ErrorBuilder {
+    pub fn format_error_string(&self) -> String {
+        let temp_str = String::from("[");
+        for err in self.0 {
+            temp_str.push_str(format!("{}, ", err));
+        }
+        temp_str.push(']');
+        temp_str
+    }
+
+    pub fn finish(self) -> Result<(), Error> {
+        if !self.0.is_empty() {
+            Err(Error::new(self.format_error_string()))?
+        }
+        Ok(())
+    }
+
+    pub fn is_valid(self) -> Result<Self, Error> {
+        if !self.0.is_empty() {
+            self.finish()?;
+        }
+        Ok(self)
+    }
+
+    pub fn no_song(self) -> Self {
+        self.0.push("no song on disk!");
+        self
+    }
+    pub fn no_image(self) -> Self {
+        self.0.push("no image on disk!");
+        self
+    }
+}
